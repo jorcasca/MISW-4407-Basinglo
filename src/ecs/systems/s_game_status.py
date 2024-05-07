716215753fbbf3f20544ec7780e12f8dbedc5070
jorcasca <jorgeeliecercastao@gmail.com>
2024-05-07 17:36:36 -0500

    new_changes

--- src/ecs/systems/s_game_status.py
+++ src/ecs/systems/s_game_status.py
@@ -8,7 +8,6 @@ def system_game_status(ecs_world: esper.World, level: dict, delta_time: float):
     bullet_fragments_components = ecs_world.get_component(CGameStatus)
     for _, (g_s) in bullet_fragments_components:
         g_s.timer -= delta_time
-        print(g_s.timer)
         if g_s.timer <= 0:
             if g_s.status == GameStatus.IDLE:
                 g_s.status = GameStatus.SHOW_ALIENS
